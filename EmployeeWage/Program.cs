3fe335e21c779db95bc0cb6099fb9eaa1ba158cf
Arun37-Kumar <arun007624@gmail.com>
2022-03-31 17:08:14 +0530

    UC1 updated

--- EmployeeWage/Program.cs
+++ EmployeeWage/Program.cs
@@ -11,6 +11,18 @@ namespace EmployeeWage
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Employee Wage Problem : ");
+            int PRESENT = 1;
+            Random random = new Random();
+            int isCheck = random.Next(0, 2);
+            if (isCheck == PRESENT)
+            {
+                Console.WriteLine("Employee is present.");
+            }
+            else
+            {
+                Console.WriteLine("Employee is absent.");
+            }
+            Console.ReadLine();
         }
     }
 }
