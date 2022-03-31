3b87b34427a3e7365cd44939349eeab51e1cc44a
Arun37-Kumar <arun007624@gmail.com>
2022-03-31 17:24:28 +0530

    Commit made

--- EmployeeWage/Program.cs
+++ EmployeeWage/Program.cs
@@ -13,8 +13,8 @@ namespace EmployeeWageProblem
             Console.WriteLine("Welcome to Employee Wage Problem : ");
 
             //Constants
-            int PART_TIME = 1;
-            int FULL_TIME = 2;
+            const int PART_TIME = 1;
+            const int FULL_TIME = 2;
             int EMP_RATE_PER_HOUR = 20;
 
             //Variables
@@ -24,20 +24,17 @@ namespace EmployeeWageProblem
 
             //Computations
             int isCheck = random.Next(0, 3);
-            if (isCheck == FULL_TIME)
+            switch (isCheck)
             {
-                Console.WriteLine("Employee is present.");
-                empHrs = 8;
-            }
-            else if (isCheck == PART_TIME)
-            {
-                Console.WriteLine("Employee is present for part time.");
-                empHrs = 8;
-            }
-            else
-            {
-                Console.WriteLine("Employee is absent.");
-                empHrs = 0;
+                case PART_TIME:
+                    empHrs = 8;
+                    break;
+                case FULL_TIME:
+                    empHrs = 8;
+                    break;
+                default:
+                    empHrs = 0;
+                    break;
             }
 
             empWage = empHrs * EMP_RATE_PER_HOUR;
